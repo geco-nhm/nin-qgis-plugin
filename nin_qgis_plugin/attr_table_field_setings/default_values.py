24d0eeaffa75f6ce84e0c682892e1a35f46c30fd
Lasse Keetz <lassekee@gmail.com>
2024-06-06 19:00:17 +0200

    Add label field, populate automatically

--- nin_qgis_plugin/attr_table_field_setings/default_values.py
+++ nin_qgis_plugin/attr_table_field_setings/default_values.py
@@ -65,6 +65,12 @@ def get_default_values(
             "default_value_expression": f"{type_fid}",
             "make_field_uneditable": True,
         },
+        {
+            "layer_name": "nin_polygons",
+            "field_name": "kode_id_label",
+            "default_value_expression": '''"grunntype_or_klenhet"''',
+            "make_field_uneditable": True,
+        },
     ]
 
     # If only one hovedtypegruppe selected, also set as default
