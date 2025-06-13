cacb342819aee1c765c4f1c01f9a5135811a679d
Adam Eindride Naas <adamen@uio.no>
2025-06-13 13:21:24 +0200

    Update book

--- publish_book.R
+++ publish_book.R
@@ -1,11 +1,20 @@
 # Set working directory to the book source
 setwd("user-guide")
 
+# In setup chunk
+is_compressed <- FALSE
+
 # Render the book (HTML GitBook version)
 bookdown::render_book("index.Rmd", output_format = "bookdown::gitbook")
 
 # Render the book (PDF version)
-bookdown::render_book("index.Rmd", output_format = "bookdown::pdf_book")
+bookdown::render_book("index.Rmd", output_format = "bookdown::pdf_book", output_dir = "../../")
+
+# In setup chunk
+is_compressed <- TRUE
+
+# Render the book in lower resolution (PDF version)
+bookdown::render_book("index.Rmd", output_format = "bookdown::pdf_book", output_dir = "C:/Users/adamen/OneDrive - Universitetet i Oslo/documents/qgis_test")
 
 # Back to the repo root
 setwd("..")
